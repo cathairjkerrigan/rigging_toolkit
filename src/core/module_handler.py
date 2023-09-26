dbf52dbd9af634cbe730a1ca80e9c246fd1d07c7
cathairjkerrigan <98622179+cathairjkerrigan@users.noreply.github.com>
2023-09-26 09:46:00 +0100

    cleaned up module_handler.py

--- src/core/module_handler.py
+++ src/core/module_handler.py
@@ -5,40 +5,8 @@ import importlib
 
 logger = getLogger(__name__)
 
-# def reload_module(module_name):
-#     try:
-#         if sys.version_info[0] == 2:
-#             # Python 2.x
-#             module = __import__(module_name)
-#             reload(module)
-#         else:
-#             # Python 3.x
-#             import importlib
-#             module = importlib.import_module(module_name)
-#             importlib.reload(module)
-#         logger.info("Module {} reloaded successfully.".format(module_name))
-        
-#         if hasattr(module, '__all__'):
-#             submodule_names = module.__all__
-#         else:
-#             submodule_names = [name for name in dir(module) if isinstance(getattr(module, name), type(module))]
-        
-#         # Recursively reload submodules
-#         for submodule_name in submodule_names:
-#             submodule = sys.modules.get('{}.{}'.format(module_name, submodule_name))
-#             if submodule:
-#                 if sys.version_info[0] == 2:
-#                     reload(submodule)
-#                 else:
-#                     importlib.reload(submodule)
-#                 logger.info("Submodule {} reloaded successfully.".format(submodule.__name__))
-#     except Exception as e:
-#         logger.error("Error reloading module {}: {}".format(module_name, e))
-
 def reload_module(module_name, include_submodules=False):
     # type: (str, bool) -> None
-
-    # print(sys)
 
     message = "Reloading {}".format(module_name)
     if include_submodules:
