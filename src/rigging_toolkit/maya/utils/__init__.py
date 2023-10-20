b026a2618aba5c8c57afa5311c092dd09b87d85f
Cathair Kerrigan <cathairjkerrigan@gmail.com>
2023-10-20 15:53:25 +0100

    updated utils imports

--- src/rigging_toolkit/maya/utils/__init__.py
+++ src/rigging_toolkit/maya/utils/__init__.py
@@ -1,8 +1,8 @@
-from .scene_utils import delete_namespaces, delete_unknown_nodes
+from .scene_utils import delete_namespaces, delete_unknown_nodes, import_asset, import_assets, get_all_transforms
 from .selection_utils import reset_attributes_to_default, unlock_unhide_keyable_attrs, lock_keyable_attrs, delete_keyframes_from_selection, select_hiearchy
 from .api import get_dag_path_api_1, get_dag_path_api_2, get_mobject
 from .deformers import deformers_by_type, clean_joint_rotation, clean_joint_rotation_for, clean_joint_rotation_for_selected, get_skin_cluster, num_influences, prune_influences
-from .mesh import get_all_shapes, toggle_template_display, query_template_display, toggle_template_display_for_all_meshes
+from .mesh_utils import get_mesh_path, get_parent, get_shapes, list_verticies, export_mesh, get_all_shapes, toggle_template_display, query_template_display, toggle_template_display_for_all_meshes
 
 __all__ = [
     "delete_namespaces",
@@ -26,5 +26,13 @@ __all__ = [
     "get_all_shapes",
     "toggle_template_display",
     "query_template_display",
-    "toggle_template_display_for_all_meshes"
+    "toggle_template_display_for_all_meshes",
+    "get_mesh_path",
+    "get_parent",
+    "get_shapes",
+    "list_verticies",
+    "export_mesh",
+    "import_asset",
+    "import_assets",
+    "get_all_transforms"
 ]
