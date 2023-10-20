39d91a3f958db4775a307f1fe5ca4fbf883491b6
Cathair Kerrigan <cathairjkerrigan@gmail.com>
2023-10-20 15:53:38 +0100

    removed mesh and added funcs to mesh_utils

--- src/rigging_toolkit/maya/utils/mesh_utils.py
+++ src/rigging_toolkit/maya/utils/mesh_utils.py
@@ -1,6 +1,11 @@
 from maya import cmds
 import logging
 from typing import List, Optional
+from pathlib import Path
+
+TEMPLATE_OFF = 0
+TEMPLATE_ON = 1
+
 
 logger = logging.getLogger(__name__)
 
@@ -21,3 +26,39 @@ def get_shapes(mesh):
     if shapes: 
         return shapes
     return None
+
+def get_mesh_path(mesh):
+    # type: (str) -> str
+    return cmds.ls(mesh, long=True)[0]
+
+def export_mesh(mesh, path):
+    # type: (str, Path) -> None
+    root = get_mesh_path(mesh)
+    cmds.AbcExport(
+        j=f"-frameRange 1 1 -uvWrite -dataFormat ogawa -root {root} -file {str(path)}"
+    )
+
+def get_all_shapes():
+    # type: () -> List
+    return cmds.ls(exactType="mesh")
+
+def query_template_display(node):
+    # type: (str) -> int
+    current_display = cmds.getAttr("{}.overrideEnabled".format(node))
+    if current_display == TEMPLATE_ON:
+        display = TEMPLATE_OFF
+    else:
+        display = TEMPLATE_ON
+    return display
+
+def toggle_template_display(node):
+    # type: (List) -> None
+    display = query_template_display(node)
+    cmds.setAttr("{}.overrideEnabled".format(node), display)
+    cmds.setAttr("{}.overrideDisplayType".format(node), display)
+    
+def toggle_template_display_for_all_meshes():
+    # type: () -> None
+    all_meshes = get_all_shapes()
+    for mesh in all_meshes:
+        toggle_template_display(mesh)
