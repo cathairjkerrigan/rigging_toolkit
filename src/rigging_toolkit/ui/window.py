112343e06349fa6b978aad7f53db4660db2850b5
Cathair Kerrigan <cathairjkerrigan@gmail.com>
2023-10-20 15:53:58 +0100

    updated window for testing

--- src/rigging_toolkit/ui/window.py
+++ src/rigging_toolkit/ui/window.py
@@ -3,6 +3,12 @@ from PySide2 import QtWidgets
 from maya.app.general.mayaMixin import MayaQWidgetDockableMixin
 
 from rigging_toolkit.maya.utils import toggle_template_display_for_all_meshes
+from rigging_toolkit.core import Context
+from rigging_toolkit.core.filesystem import has_folders, find_latest
+from pathlib import Path
+from maya import cmds
+from rigging_toolkit.maya.shaders.build_shaders import PBRShader, setup_textures
+from rigging_toolkit.maya.assets.asset_manager import export_all_character_assets
 
 import logging
 
@@ -35,10 +41,35 @@ class RiggingToolboxWindow(MayaQWidgetDockableMixin, QtWidgets.QWidget):
 
         self.test_button = QtWidgets.QPushButton("Test Button")
 
+        self.test_button_2 = QtWidgets.QPushButton("Test Button 2")
+
         self._layout.addWidget(self.test_button)
+        self._layout.addWidget(self.test_button_2)
 
         self.test_button.clicked.connect(self.test_func)
+        self.test_button_2.clicked.connect(self.test_func_2)
+
+        self.context = Context.new(
+            project_path=Path(r"F:\university_work\major_project\characters"),
+            character_name="kate",
+            create_context=True
+        )
 
     def test_func(self):
-        logger.info("Button test")
-        toggle_template_display_for_all_meshes()
+
+        print(self.context.character_name)
+        print(self.context.character_path)
+        print(self.context.animation_path)
+        print(self.context.assets_path)
+        print(self.context.texture_path)
+
+        for asset in self.context.assets_path.iterdir():
+            path = asset / "meshes"
+            name = f"geo_{asset.name}_L1"
+            latest, _ = find_latest(path, name, "abc")
+            cmds.file(str(latest), i=True, uns=False)
+
+        setup_textures(self.context)
+
+    def test_func_2(self):
+        export_all_character_assets(self.context)
